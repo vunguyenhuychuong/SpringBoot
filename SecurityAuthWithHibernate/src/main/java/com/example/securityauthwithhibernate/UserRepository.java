1647380e3cd116dd71676923a8df7f6477862aad
Chương Vũ <86512368+vunguyenhuychuong@users.noreply.github.com>
2022-09-07 09:28:54 +0700

    Add files via upload

---
+++ SecurityAuthWithHibernate/src/main/java/com/example/securityauthwithhibernate/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.securityauthwithhibernate;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUserName(@Param("username") String username);
+}
---
+++ SecurityAuthWithHibernate/src/test/java/com/example/securityauthwithhibernate/SecurityAuthWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityauthwithhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAuthWithHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
